7b0a59cade529604b2fcf6827eff8494d99830e4
Justin Pille <justinpille@Justins-MBP.example.org>
2019-09-20 15:12:56 -0500

    add a memoize lib

---
+++ client/src/libs/memoize/createCache.ts
@@ -0,0 +1,41 @@
+export const createCache = ({
+  maxAge,
+  maxEntries
+}: {
+  maxAge?: number;
+  maxEntries?: number;
+} = {}) => {
+  const cache = new Map<string, any>();
+  const timerIds: number[] = [];
+
+  return {
+    memo<T>(key: string, fn: () => T) {
+      if (cache.has(key)) {
+        return cache.get(key) as T;
+      }
+      const result = fn();
+      cache.set(key, result);
+
+      // handle maxSize
+      if (maxEntries && cache.size > maxEntries) {
+        const firstKey = [...cache.keys()][0];
+        cache.delete(firstKey);
+      }
+
+      // handle maxAge
+      if (maxAge) {
+        timerIds.push(
+          window.setTimeout(() => {
+            cache.delete(key);
+          }, maxAge)
+        );
+      }
+
+      return result;
+    },
+    clearCache() {
+      timerIds.forEach(window.clearTimeout);
+      cache.clear();
+    }
+  };
+};
